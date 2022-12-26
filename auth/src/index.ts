44519552bff6dc3495457ff82b3085545e7357a2
senamix <badaowap@gmail.com>
2022-12-26 10:37:06 +0900

    build user model and setup mongdodb

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import 'express-async-errors';
-import { json } from 'body-parser';
+import mongoose from 'mongoose'
 
+import { json } from 'body-parser';
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
@@ -25,6 +26,17 @@ app.all('*', async (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!!!!!!!!');
-});
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('Connected to Mongodb');
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000...');
+  });
+}
+
+start();
